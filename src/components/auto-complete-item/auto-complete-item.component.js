d091878eb72c90d3e1d78301c5049a6fc820468b
changye <changye@thinkerx.com>
2016-10-09 14:22:32 +0800

    新增cs-auto-complete-item组件

---
+++ src/components/auto-complete-item/auto-complete-item.component.js
@@ -0,0 +1,21 @@
+/**
+ * @author changye@thinkerx.com
+ */
+angular
+    .module('athena.autoComplete', [])
+    .component('autoCompleteItem', {
+        //templateUrl: function (element, attrs) {
+        //    console.log(999,    element.type);
+        //    return attrs.templateUrl || 'views/components/cs-auto-complete-' + attrs.type + '.view.html';
+        //},
+        template: '<div ng-include="getTemplate()"></div>',
+        controller: 'AutoCompleteItemCtrl',
+        controllerAs: 'complete',
+        bindings: {
+            type: '<',
+            itemList: '=',
+            selectedItem: '=',
+            isPopup: '='
+        }
+    });
+
---
+++ src/components/auto-complete-item/auto-complete-item.controller.js
@@ -0,0 +1,25 @@
+/**
+ * @author changye@thinkerx.com
+ */
+angular
+    .module('athena.autoComplete')
+    .controller('AutoCompleteItemCtrl', AutoCompleteItemCtrl);
+
+AutoCompleteItemCtrl.$inject = ['$scope'];
+
+function AutoCompleteItemCtrl($scope) {
+    var vm = this;
+
+    vm.selectResult = selectResult;
+
+    $scope.getTemplate = function () {
+        return 'views/components/auto-complete-' + vm.type +  '.view.html';
+    };
+
+    function selectResult(item) {
+        vm.selectedItem = item;
+        $scope.$emit('get_auto_complete_item', item);
+        vm.isPopup = false;
+    }
+
+}
