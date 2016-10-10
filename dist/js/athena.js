61fe95caf16ad3535a6a03ceb7e9a5a7cbf5600a
ZhangBoxuan <821836281@qq.com>
2016-10-10 10:51:41 +0800

    添加多级菜单demo

--- dist/js/athena.js
+++ dist/js/athena.js
@@ -1,5 +1,6 @@
 angular.module('athena', [
-    'athena.autoComplete'
+    'athena.autoComplete',
+    'athena.menuSelector'
 ]);
 
 /**
@@ -53,6 +54,105 @@ function AutoCompleteCtrl($scope) {
         }
         vm.isPopup = keywords ? true : false;
     }
+}
+
+/**
+ * @author zhangboxuan@thinkerx.com
+ */
+angular
+    .module('athena.menuSelector', [])
+    .component('menuSelector',{
+        templateUrl: 'views/menu-selector.view.html',
+        controller: 'MenuSelector',
+        controllerAs: 'menu',
+        bindings: {
+            menuList: '<',
+            placeholder: '@'
+        }
+    });
+
+/**
+ * @author zhangboxuan@thinkerx.com
+ */
+angular
+    .module('athena.menuSelector')
+    .controller('MenuSelector', MenuSelector);
+
+MenuSelector.$inject = [];
+
+function MenuSelector() {
+    var vm = this;
+
+    vm.selectedMenu = [];
+    vm.isShow = false;
+
+    vm.selectMenu = selectMenu;
+    vm.showSelectedMenu = showSelectedMenu;
+    vm.hoverSelectMenu = hoverSelectMenu;
+
+    init();
+
+    function init() {
+
+    }
+
+    function selectMenu(menuList1, menuList2, menuList3) {
+        if(menuList3 && menuList2) {
+            vm.selectedMenu = [menuList1, menuList2, menuList3];
+            vm.isShow = false;
+        } else if(!menuList3 && menuList2 && menuList2.children.length === 0) {
+            vm.selectedMenu = [menuList1, menuList2];
+            vm.isShow = false;
+        } else if(!menuList3 && !menuList2 && menuList1.children.length === 0) {
+            vm.selectedMenu = [menuList1];
+            vm.isShow = false;
+        }
+    }
+
+    function showSelectedMenu() {
+        vm.isShow = !vm.isShow;
+        vm.menuList1 = vm.selectedMenu[0];
+        vm.menuList2 = vm.selectedMenu[1];
+        vm.menuList3 = vm.selectedMenu[2];
+    }
+
+    function hoverSelectMenu(item) {
+        switch(item.level) {
+            case 1:
+                vm.menuList1 = item;
+                break;
+            case 2:
+                vm.menuList2 = item;
+                break;
+            case 3:
+                vm.menuList3 = item;
+                break;
+        }
+    }    
+}
+
+/**
+ * 多级菜单过滤器
+ * @author zhangboxuan@thinkerx.com
+ */
+angular
+    .module('athena.menuSelector')
+    .filter('menuSelector', menuSelector);
+
+menuSelector.$inject = [];
+
+function menuSelector() {
+    return function(input) {
+        if(input) {
+            if(input[2]) {
+                return input[0].name + ' > ' + input[1].name + ' > ' + input[2].name;
+            } else if(input[1] && input[1].children.length == 0) {
+                return input[0].name + ' > ' + input[1].name;
+            } else if(input[0] && input[0].children.length == 0) {
+                return input[0].name;
+            }
+        }            
+    };
 }
 
 /**
