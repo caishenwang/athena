cb3a24e38936d6c6ad7756675a7d19ad02237c80
ZhangBoxuan <821836281@qq.com>
2016-10-14 15:39:15 +0800

    需要更新bower，提交dist

--- dist/js/athena.js
+++ dist/js/athena.js
@@ -338,7 +338,9 @@ angular
         controllerAs: 'menu',
         bindings: {
             menuList: '<',
-            placeholder: '@'
+            placeholder: '@',
+            level: '<',
+            selectedMenu: '<'
         }
     });
 
@@ -353,6 +355,8 @@ MenuSelector.$inject = ['$scope', '$timeout'];
 
 function MenuSelector($scope, $timeout) {
     var vm = this;
+    var level1ScrollTop = 0;
+    var level2ScrollTop = 0;
 
     vm.selectedMenu = [];
     vm.isShow = false;
@@ -361,20 +365,14 @@ function MenuSelector($scope, $timeout) {
     vm.showSelectedMenu = showSelectedMenu;
     vm.hoverSelectMenu = hoverSelectMenu;
 
-    init();
-
-    function init() {
-
-    }
-
     function selectMenu(menuList1, menuList2, menuList3) {
-        if(menuList3 && menuList2) {
+        if(menuList3 && menuList2 && vm.level === 3) {
             vm.selectedMenu = [menuList1, menuList2, menuList3];
             vm.isShow = false;
-        } else if(!menuList3 && menuList2 && menuList2.children.length === 0) {
+        } else if((!menuList3 && menuList2 && menuList2.children.length === 0) || (vm.level === 2 && menuList2)) {
             vm.selectedMenu = [menuList1, menuList2];
             vm.isShow = false;
-        } else if(!menuList3 && !menuList2 && menuList1.children.length === 0) {
+        } else if((!menuList3 && !menuList2 && menuList1.children.length === 0) || vm.level === 1) {
             vm.selectedMenu = [menuList1];
             vm.isShow = false;
         }
@@ -383,28 +381,42 @@ function MenuSelector($scope, $timeout) {
 
     function showSelectedMenu() {
         vm.isShow = !vm.isShow;
-        vm.menuList1 = vm.selectedMenu[0];
-        vm.menuList2 = vm.selectedMenu[1];
-        vm.menuList3 = vm.selectedMenu[2];
+        if(vm.selectedMenu) {
+            vm.menuList1 = vm.selectedMenu[0];
+            vm.menuList2 = vm.selectedMenu[1];
+            vm.menuList3 = vm.selectedMenu[2];
+        }
     }
 
     function hoverSelectMenu(item, $event) {
         /*
          * position
          */
+        var currentTargetTop = $event.currentTarget.offsetTop;
+        var level1 = angular.element('.level-1');
+        var level2 = angular.element('.level-2');
+        var level3 = angular.element('.level-3');
+
+        level1.find('ul').scroll(function(){
+            level1ScrollTop = level1.find('ul').scrollTop();
+        });
+        level2.find('ul').scroll(function(){
+            level2ScrollTop = level2.find('ul').scrollTop();
+        });
+
         var className = $event.currentTarget.parentNode.className;
         if(new RegExp('level-1').test(className)) {
             $timeout(function() {
-                $('.level-2').css({
-                    'margin-top': $event.currentTarget.offsetTop
-                });
+                if(level2.length !== 0) {
+                    level2[0].style.marginTop = currentTargetTop - level1ScrollTop - 6 + 'px';
+                }
             });
         }
         if(new RegExp('level-2').test(className)) {
             $timeout(function() {
-                $('.level-3').css({
-                    'margin-top': $event.currentTarget.offsetTop
-                });
+                if(level3.length !== 0) {
+                    level3[0].style.marginTop = currentTargetTop - level2ScrollTop - 6 + 'px';
+                }
             });
         }
     }
@@ -425,9 +437,9 @@ function menuSelector() {
         if(input) {
             if(input[2]) {
                 return input[0].name + ' > ' + input[1].name + ' > ' + input[2].name;
-            } else if(input[1] && input[1].children.length === 0) {
+            } else if(input[1]) {
                 return input[0].name + ' > ' + input[1].name;
-            } else if(input[0] && input[0].children.length === 0) {
+            } else if(input[0]) {
                 return input[0].name;
             }
         }            
